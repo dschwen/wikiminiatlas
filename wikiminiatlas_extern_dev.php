79582b5f8218b456ba57e799d977eb30c66b72a4
Daniel Schwen <toolserver@schwen.de>
2011-03-30 22:16:58 +0000

    ev.pageX/Y is already normalized by jQuery

--- wikiminiatlas_extern_dev.php
+++ wikiminiatlas_extern_dev.php
@@ -605,14 +605,7 @@ function wmaKeypress(ev)
 }
 
 function wmaMouseCoords(ev) {
- if(ev.pageX || ev.pageY)
- {
-  return {x:ev.pageX, y:ev.pageY};
- }
- return {
-  x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-  y:ev.clientY + document.body.scrollTop  - document.body.clientTop
- };
+ return {x:ev.pageX, y:ev.pageY};
 }
 
 function wmaGetDataURL(y,x,z) {
