23b814d2de84e19ccbcada9fe68442bf3ba50d36
Daniel Schwen <daniel@schwen.de>
2012-02-20 19:37:15 -0700

    start working on query decoder

--- label_dev.php
+++ label_dev.php
@@ -5,6 +5,10 @@ $lang=$_GET['l'];
 $y=floatval($_GET['a']);
 $x=floatval($_GET['b']);
 $z=intval($_GET['z']);
+
+// experimental compressed query
+$y=floatval($_GET['r']);
+
 //$rev=intval($_GET['rev']);
 
 // get language id
@@ -23,18 +27,28 @@ $rev = $lrev[$lang];
 
 $wikiminiatlas_zoomsize = array( 3.0, 6.0 ,12.0 ,24.0 ,48.0, 96.0, 192.0, 384.0, 768.0, 1536.0,  3072.0, 6144.0, 12288.0, 24576.0, 49152.0, 98304.0 );
 
-// hotspot coordinates for the map symbols
-$ix = array( 0, 0, 5, 0, 0, 2, 3, 4, 5, 6, 6 );
-$iy = array( 0, 0, 8, 0, 0, 2, 3, 4, 5, 6, 6 );
-
 $ts_pw = posix_getpwuid(posix_getuid());
 $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/.my.cnf");
 $db = mysql_connect($lang.'wiki-p.db.toolserver.org', $ts_mycnf['user'], $ts_mycnf['password']);
 unset($ts_mycnf, $ts_pw);
 mysql_select_db($lang.'wiki_p', $db);
 
-$query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style from  page p, u_dschwen.wma_tile t, u_dschwen.wma_connect c, u_dschwen.wma_label l  WHERE l.lang_id='$l' AND c.rev='$rev' AND c.tile_id=t.id AND t.x='$x' AND c.label_id=l.id  AND t.y='$y' AND t.z='$z' AND p.page_id = l.page_id AND c.tile_id = t.id AND page_namespace=0 AND l.page_id=p.page_id;";
-//echo $query,"\n";
+$co = Array('x','y');
+if( $r != NULL ) {
+  $q = Array();
+
+  // decode compressed query
+  $qi = explode("|",$r);
+  foareach( $qi as $i ) {
+    $qp = explode(",",$i);
+    // = or >= <=
+  }
+
+  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style from  page p, u_dschwen.wma_tile t, u_dschwen.wma_connect c, u_dschwen.wma_label l  WHERE l.lang_id='$l' AND c.rev='$rev' AND c.tile_id=t.id AND (".") AND c.label_id=l.id AND t.z='$z' AND p.page_id = l.page_id AND c.tile_id = t.id AND page_namespace=0 AND l.page_id=p.page_id;";
+  echo $query,"\n";
+} else {
+  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style, t.x as dx, t.y as dy from  page p, u_dschwen.wma_tile t, u_dschwen.wma_connect c, u_dschwen.wma_label l  WHERE l.lang_id='$l' AND c.rev='$rev' AND c.tile_id=t.id AND t.x='$x' AND c.label_id=l.id  AND t.y='$y' AND t.z='$z' AND p.page_id = l.page_id AND c.tile_id = t.id AND page_namespace=0 AND l.page_id=p.page_id;";
+}
 $res = mysql_query( $query );
 
 $ymin = (180*$y)/$wikiminiatlas_zoomsize[$z] - 90.0;
@@ -55,8 +69,10 @@ while( $row = mysql_fetch_array( $res) )
     "tx"    => $tx,
     "ty"    => $ty,
     "name"  => $row['name']
+    "dx"  => $row['dx']
+    "dy"  => $row['dy']
   );
 }
 mysql_close( $db );
-echo json_encode( array( "label" => $items) );
+echo json_encode( array( "label" => $items, "z" => $z ) );
 ?>
