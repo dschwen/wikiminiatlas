0421a172a892601c8f85158966ffa632f05f0810
Daniel Schwen <daniel@schwen.de>
2012-07-23 19:32:50 +0000

    work on json tile extraction

--- jsontile.php
+++ jsontile.php
@@ -2,11 +2,27 @@
 
 $dbconn = pg_connect("host=sql-mapnik dbname=osm_mapnik");
 
+$x = intval($_GET['x']);
+$y = intval($_GET['y']);
+$z = intval($_GET['z']);
+
+// only reply for high zoomlevels!
+if( $z < 12 ) exit;
+
+// padding in pixels
+$pad = 10;
+
+// coordinates of upper right and lower left corners
 $urx = 11.0;
 $ury = 49.01;
 $llx = 10.99;
 $lly = 49.0;
 
+// size of zoom level in tiles
+$mx = 3 * ( 2 << $z );
+$my = $mx/2;
+
+// build query for the cropped data
 $query = "
   select 
     ST_AsGeoJSON(
@@ -22,8 +38,8 @@ $query = "
       transform( ST_GeomFromText('POLYGON(($llx $ury, $urx $ury, $urx $lly, $llx $lly, $llx $ury))', 4326 ), 900913 ) 
     );
 ";
-      //ST_GeomFromText('POLYGON(($llx $ury, $urx $ury, $llx $llr, $lrx $lry, $ulx $uly))', 4326 )) AS bbox_nodes;
 
+// perform query
 $result = pg_query($dbconn, $query);
 if( !$result ) {
   echo pg_last_error($dbconn);
