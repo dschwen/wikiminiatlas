befe89d463c60c402bc0eb318759de3550c63942
Daniel Schwen <daniel@schwen.de>
2013-02-08 16:58:43 +0000

    small utils file

---
+++ utils.js
@@ -0,0 +1,14 @@
+/**
+ * Provides requestAnimationFrame in a cross browser way.
+ */
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+           window.setTimeout(callback, 1000/60);
+         };
+})();
+
