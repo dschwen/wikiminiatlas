4eb7a2cb39c7dcfca775f9c5a0229ce7c6c02448
Daniel Schwen <daniel@schwen.de>
2013-02-08 21:20:37 +0000

    list 3rd party libs

--- wikiminiatlas_extern_dev.php
+++ wikiminiatlas_extern_dev.php
@@ -4,30 +4,24 @@ header('Content-type: text/javascript');
 ?>
 /************************************************************************
  *
- * WikiMiniAtlas (c) 2006-2010 by Daniel Schwen
+ * WikiMiniAtlas (c) 2006-2013 by Daniel Schwen, licensed under GPLv3
  *  Script to embed interactive maps into pages that have coordinate templates
- *  also check my commons page [[:commons:User:Dschwen]] for more tools
+ *  see:
+ *  * http://meta.wikimedia.org/wiki/WikiMiniAtlas
+ *  * https://github.com/dschwen/wikiminiatlas
  *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * WMA contains the following third party liraries:
+ *  * http://jquery.org/ (MIT license)
+ *  * https://github.com/douglascrockford/JSON-js (PD)
+ *  * https://github.com/toji/gl-matrix/blob/master/LICENSE.md 
+ *  * Poly2Tra, Copyright (c) 2009-2010, Poly2Tri Contributors
+ *    http://code.google.com/p/poly2tri/
  *
  ************************************************************************/
 
 // include files
 <? 
-require( 'min/jquery.min.js');
+require( 'min/jquery.min.js' );
 require( 'min/json2.min.js' ); 
 require( 'min/utils.min.js' ); 
 require( 'min/glMatrix-0.9.5.custom.js' ); 
