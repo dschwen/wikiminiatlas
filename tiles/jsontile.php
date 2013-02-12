fcf7d108973d9f5ba075b2dd3fc15aa496b458c4
Daniel Schwen <daniel@schwen.de>
2013-02-12 04:10:22 +0000

    enforce minimum tile version

--- tiles/jsontile.php
+++ tiles/jsontile.php
@@ -16,9 +16,13 @@ if( $a!=='query' && $a!=='print' ) {
   if( $a !== 'purge' ) {
     if( file_exists( $tfile ) ) {
       header("Cache-Control: public, max-age=3600");
-      // TODO parse json and check timestamp (or check file time)
-      readfile( $tfile );
-      exit;
+      // parse json and check tile version ( or timestamp, or check file time)
+      $f = file_get_contents($tfile);
+      $d = json_decode($f);
+      if( $d->v >= 4 ) {
+        echo $f;
+        exit;
+      }
     }
   }
 }
