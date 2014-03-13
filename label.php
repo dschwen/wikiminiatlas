61c67b4917c344f4f39f4377e1fa1f500c6b3adb
Daniel Schwen <daniel@schwen.de>
2014-03-13 22:09:32 +0000

    fix undef var bugs

--- label.php
+++ label.php
@@ -13,11 +13,15 @@ $z=intval($_GET['z']);
 //$rev=intval($_GET['rev']);
 
 // globe parameter (defaults to Earth)
-$g=$_GET['g'];
-if(!isset($g)) $g="Earth";
+$g="Earth";
+if (array_key_exists('g', $_GET)) $g=$_GET['g'];
 
-// experimental compressed query
-$r=$_GET['r'];
+// experimental range query
+$r = NULL;
+if (array_key_exists('r', $_GET)) $r=$_GET['r'];
+
+// Uncomment briefly to clear the memory cahce
+//apc_clear_cache();
 
 // APC - query cache
 $key = md5($x.'|'.$y.'|'.$z.'|'.$lang.'|'.$r);
@@ -29,9 +33,16 @@ if ($result = apc_fetch($key)) {
 // get language id
 $alllang=explode(',',"ar,bg,ca,ceb,commons,cs,da,de,el,en,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,ht,hu,id,it,ja,ko,lt,ms,new,nl,nn,no,pl,pt,ro,ru,simple,sk,sl,sr,sv,sw,te,th,tr,uk,vi,vo,war,zh,af,als,be,bpy,fy,ga,hy,ka,ku,la,lb,lv,mk,ml,nds,nv,os,pam,pms,ta,vec,kk,ilo,ast,uz,oc,sh,tl");
 $l = array_search( $lang, $alllang );
-if( $l === FALSE ) {
+if ($l===FALSE) {
   echo "";
   exit;
+}
+
+// set proper namespace for the query below
+if ($lang=='commons') {
+  $namespace=6;
+} else {
+  $namespace=0;
 }
 
 // load table of most current revisions
@@ -44,31 +55,32 @@ $wikiminiatlas_zoomsize = array( 3.0, 6.0 ,12.0 ,24.0 ,48.0, 96.0, 192.0, 384.0,
 
 // connect to database
 $ts_pw = posix_getpwuid(posix_getuid());
-$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/.my.cnf");
-$db = mysqli_connect('p:localhost', $ts_mycnf['user'], $ts_mycnf['password'], $lang."wiki.labsdb");
+$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
+$db = mysqli_connect('p:'.$lang."wiki.labsdb", $ts_mycnf['user'], $ts_mycnf['password'], $lang."wiki_p");
 unset($ts_mycnf, $ts_pw);
 
 $g=mysqli_real_escape_string($db, $g);
 
-if( $r != NULL ) {
+if ($r!=NULL) {
+  $r = $_GET['r'];
   $co = Array('x','y');
   $q = Array();
   $n = 0;
 
   // decode compressed query
   $qi = explode("|",$r);
-  if( ( count($qi) < 1 ) || ( count($qi) > 10 ) ) exit;
-  foreach( $qi as $i ) {
+  if ((count($qi)<1) || (count($qi)>10)) exit;
+  foreach ($qi as $i) {
     $qp = explode(",",$i);
-    if( count($qp) < 1 || count($qp) > 2 ) exit;
+    if (count($qp) < 1 || count($qp) > 2) exit;
     $s=""; $n2=1;
-    for($j=0;$j<2;$j++) { 
+    for ($j=0; $j<2; $j++) { 
       // = or >= <=
       $k=explode("-",$qp[$j]);
-      if( count($k) == 1 ) {
+      if (count($k) == 1) {
         $s .= "t.".$co[$j]."=".intval($k[0]);
       } 
-      else if( count($k) == 2 ){
+      else if (count($k) == 2) {
         $s .= "t.".$co[$j].">=".intval($k[0])." AND t.".$co[$j]."<=".intval($k[1]);
         $n2 *= max(0,intval($k[1])-intval($k[0]));
       }
@@ -86,15 +98,15 @@ if( $r != NULL ) {
     echo "Requesting too many tiles!";
     exit;
   }
-  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style, t.x as dx, t.y as dy, l.weight as wg, l.page_id as id from  page p, p50380g50921__wma_p.wma_tile t, p50380g50921__wma_p.wma_connect c, p50380g50921__wma_p.wma_label l  WHERE l.lang_id='$l' AND l.globe='$g' AND c.rev='$rev' AND c.tile_id=t.id AND ( ".implode(" OR ",$q)." ) AND c.label_id=l.id AND t.z='$z' AND c.tile_id = t.id AND page_namespace='$namespace' AND l.page_id=p.page_id;";
+  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style, t.x as dx, t.y as dy, l.weight as wg, l.page_id as id from  page p, p50380g50921__wma_p.wma_tile t, p50380g50921__wma_p.wma_connect c, p50380g50921__wma_p.wma_label l  WHERE l.lang_id='$l' AND l.globe='$g' AND c.rev='$rev' AND c.tile_id=t.id AND ( ".implode(" OR ",$q)." ) AND c.label_id=l.id AND t.z='$z' AND c.tile_id = t.id AND page_namespace='$namespace' AND l.page_id=p.page_id";
 } else {
-  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style, t.x as dx, t.y as dy, l.weight as wg, l.page_id as id from  page p, p50380g50921__wma_p.wma_tile t, p50380g50921__wma_p.wma_connect c, p50380g50921__wma_p.wma_label l  WHERE l.lang_id='$l' AND  l.globe='$g' AND c.rev='$rev' AND c.tile_id=t.id AND t.x='$x' AND c.label_id=l.id  AND t.y='$y' AND t.z='$z' AND c.tile_id = t.id AND page_namespace='$namespace' AND l.page_id=p.page_id;";
+  $query = "select p.page_title as title, l.name as name, l.lat as lat, l.lon as lon, l.style as style, t.x as dx, t.y as dy, l.weight as wg, l.page_id as id from  page p, p50380g50921__wma_p.wma_tile t, p50380g50921__wma_p.wma_connect c, p50380g50921__wma_p.wma_label l  WHERE l.lang_id='$l' AND  l.globe='$g' AND c.rev='$rev' AND c.tile_id=t.id AND t.x='$x' AND c.label_id=l.id  AND t.y='$y' AND t.z='$z' AND c.tile_id = t.id AND page_namespace='$namespace' AND l.page_id=p.page_id";
 }
 
 $res = mysqli_query($db, $query);
 
 $items = array();
-while( $row = mysqli_fetch_assoc($res) )
+while ($row = mysqli_fetch_assoc($res))
 {
   $x = $row['dx'];
   $y = $row['dy'];
@@ -109,7 +121,7 @@ while( $row = mysqli_fetch_assoc($res) )
   $fx = ( ( ( $row["lon"] - $xmin ) * $wikiminiatlas_zoomsize[$z] * 128) / 180.0 ) - $tx;
   $s = $row['style'];
 
-  if( $lang == "commons" ) {
+  if ($lang=="commons") {
     $n = explode( '|', $row["name"], 4 );
     $w = $n[0];
     $h = $n[1];
@@ -145,7 +157,7 @@ while( $row = mysqli_fetch_assoc($res) )
     );
   }
 } // TODO only send fx,fy,wg for max label zoom!
-mysqli_close( $db );
+mysqli_close($db);
 
 //header("Content-type: application/json");
 header("Cache-Control: public, max-age=3600");
