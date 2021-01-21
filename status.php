c5da6632a35154378618df818a6591a60129b33a
Daniel Schwen <daniel@schwen.de>
2021-01-21 20:36:35 +0000

    add status script

---
+++ status.php
@@ -0,0 +1,34 @@
+<?php
+
+//
+// label.php version for WMF Cloud, (c) 2021 Daniel Schwen
+//
+
+require 'master.inc';
+
+// no errors
+error_reporting(0);
+?>
+
+<html>
+<head><title></title></head>
+<body>
+
+<h2>Label revisions</h2>
+<table>
+
+<?php
+foreach ($lrev as $lang => $rev)
+{
+?>
+
+<tr>
+<td><a href="https://<?= $lang ?>.<?= $lang == 'commons' ? 'wikimedia' : 'wikipedia' ?>.org/"><?= $lang ?></a></td>
+<td><?= $rev ?></td>
+</tr>
+
+<?php
+}
+?>
+
+</table>
